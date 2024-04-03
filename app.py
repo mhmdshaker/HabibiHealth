4c36fbe50933de4ffc68e9705e6e0d330ab3ddc7
raghid03 <raa183@mail.aub.edu>
2024-04-03 23:28:08 +0300

    authentication and token added

--- app.py
+++ app.py
@@ -34,5 +34,46 @@ def user():
     return jsonify(user_schema.dump(new_user))
 
 
+def create_token(user_id):
+    payload = {
+    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=4),
+    'iat': datetime.datetime.utcnow(),
+    'sub': user_id
+    }
+    return jwt.encode(
+    payload,
+    SECRET_KEY,
+    algorithm='HS256'
+    )
+
+@app.route('/authentication', methods=['POST'])
+def authentication():
+    user_name=request.json["user_name"]
+    password=request.json["password"]
+
+    if not user_name or not password:
+        return abort(400)
+    user=User.query.filter_by(user_name=user_name).first()
+    if user is None:
+        return abort(403)
+    if not bcrypt.check_password_hash(user.hashed_password,password):
+        return abort(403)
+    else:
+        return jsonify(
+            {"token":create_token(user.id)}
+        )
+    
+def extract_auth_token(authenticated_request):
+    auth_header = authenticated_request.headers.get('Authorization')
+    if auth_header:
+        return auth_header.split(" ")[1]
+    else:
+        return None
+
+def decode_token(token):
+    payload = jwt.decode(token, SECRET_KEY, 'HS256')
+    return payload['sub']
+
+
 
 
