14f528c03b71745c6d35f511e81693b968512135
khalilbitarAUB <kmb27@mail.aub.edu>
2024-04-04 01:38:25 +0300

    added exercise model

---
+++ model/exercise.py
@@ -0,0 +1,18 @@
+from ..app import db, ma
+
+
+class Exercise(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    calories_burnt = db.Column(db.Integer, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+                        nullable=False)
+    def __init__(self, name, calories_burnt):
+        super(Exercise, self).__init__(name=name, calories_burnt=calories_burnt)
+
+
+class user_schema(ma.Schema):
+ class Meta:
+    fields = ("id", "name", "calories_burnt")
+    model = Exercise
+user_schema = user_schema()
