e388dad002e0783dfc15f171c8d2e7afc7cd5682
khalilbitarAUB <kmb27@mail.aub.edu>
2024-04-04 02:28:38 +0300

    changed calories to Float datatype

--- model/exercise.py
+++ model/exercise.py
@@ -4,7 +4,7 @@ from ..app import db, ma
 class Exercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
-    calories_burnt = db.Column(db.Integer, unique=False)
+    calories_burnt = db.Column(db.Float, unique=False)
     
     def __init__(self, name, calories_burnt):
         super(Exercise, self).__init__(name=name, calories_burnt=calories_burnt)
--- model/food.py
+++ model/food.py
@@ -4,7 +4,7 @@ from ..app import db, ma
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
-    calories = db.Column(db.Integer, unique=False)
+    calories = db.Column(db.Float, unique=False)
    
     def __init__(self, name, calories):
         super(Food, self).__init__(name=name, calories=calories)
