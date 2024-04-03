61092999bbd82dac53b80dcfa2e80b221dfc5409
raghid03 <raa183@mail.aub.edu>
2024-04-03 21:54:40 +0300

    first push

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,request,jsonify,abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+import jwt
+from flask_cors import cross_origin
+
+from .db_config import DB_CONFIG
+
+SECRET_KEY = "b'|\xe7\xbfU3`\xc4\xec\xa7\xa9zf:}\xb5\xc7\xb9\x139^3@Dv'"
+from datetime import timedelta
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONFIG
+ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
+CORS(app)
+db = SQLAlchemy(app)
+  
+from .model.user import User, user_schema
+
+
+
+
